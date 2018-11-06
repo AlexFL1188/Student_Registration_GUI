98fc860788413ff624c03be7e94b4e744772641d
Alexander <tr3821ep@my.century.edu>
2018-11-05 21:25:52 -0600

    worked on new student creation

--- src/edu/century/groupProject/DriverGUI.java
+++ src/edu/century/groupProject/DriverGUI.java
@@ -1,20 +1,24 @@
 package edu.century.groupProject;
 
-import edu.century.groupProject.collections.CourseCollection;
-import edu.century.groupProject.collections.StudentCollection;
-
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.UIManager;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 
@@ -24,7 +28,8 @@ public class DriverGUI extends JFrame implements ActionListener{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	private JTextField fullNameTXTField;
+	private JTextField birthDateTXTField;
 	private JPanel contentPane;
 	private JTextArea outputArea;
 	
@@ -96,7 +101,7 @@ public class DriverGUI extends JFrame implements ActionListener{
     		System.exit(0);
     	}else {//if new student create new JFrame for creating new student
     		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    		setExtendedState(JFrame.MAXIMIZED_BOTH); 
+    		setExtendedState(JFrame.NORMAL); 
     		setBounds(500, 500, 500, 500);
     		contentPane = new JPanel();
     		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
@@ -106,6 +111,39 @@ public class DriverGUI extends JFrame implements ActionListener{
     		JPanel topPanel = new JPanel();
     		contentPane.add(topPanel);
     		topPanel.setLayout(new GridLayout(0,1,20,20));
+    		
+    		//label for fullName
+    		JLabel fullNameLabel = new JLabel("Full Name:");
+    		topPanel.add(fullNameLabel);
+    		
+    		//fullName text input area
+    		fullNameTXTField = new JTextField();
+    		GridBagConstraints fullNameGrid = new GridBagConstraints();
+    		fullNameGrid.fill = GridBagConstraints.HORIZONTAL;
+    		fullNameGrid.insets = new Insets(0, 0, 5, 5);
+    		fullNameGrid.gridx = 1;
+    		fullNameGrid.gridy = 0;
+    		topPanel.add(fullNameTXTField, fullNameGrid);
+    		fullNameTXTField.setColumns(10);
+    		
+    		//label for birthDate
+    		JLabel birthDateLabel = new JLabel("BirthDate (MM/DD/YYYY) :");
+    		topPanel.add(birthDateLabel);
+    		
+    		//birthDate text input area
+    		birthDateTXTField = new JTextField();
+    		GridBagConstraints birthDateGrid = new GridBagConstraints();
+    		birthDateGrid.insets = new Insets(0, 0, 5, 0);
+    		birthDateGrid.fill = GridBagConstraints.HORIZONTAL;
+    		birthDateGrid.gridx = 3;
+    		birthDateGrid.gridy = 0;
+    		topPanel.add(birthDateTXTField, birthDateGrid);
+    		birthDateTXTField.setColumns(10);
+    		
+    		//button to compute a user defined period of Pay Checks
+    		JButton btnRegister = new JButton("Register");
+    		btnRegister.addActionListener(this);
+    		topPanel.add(btnRegister);
 		
     		//text area to display info to user
     		JPanel bottomPanel = new JPanel();
@@ -118,8 +156,18 @@ public class DriverGUI extends JFrame implements ActionListener{
 	//action method to determine what buttons do what
 		public void actionPerformed(ActionEvent e) {
 			String nameOfCallingBtn = e.getActionCommand();
-			if() {
-			
+			if(nameOfCallingBtn.equals("Register")) {
+			    SimpleDateFormat format = new SimpleDateFormat("mm/DD/yyyy");
+			    GregorianCalendar cal1 = new GregorianCalendar();
+			    try {
+					cal1.setTime(format.parse(birthDateTXTField.getText()));
+				} catch (ParseException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				Student s1 = new Student(fullNameTXTField.getText(), cal1);
+				outputArea.append("Thank you for your Registration!!");
+				outputArea.append(s1.toString());
 			} 
 			else if (nameOfCallingBtn.equals("Clear Console")) {
 				clearConsole();
