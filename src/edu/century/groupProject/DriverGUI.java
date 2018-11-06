3bcc669e4a60aee050d2f44009667ec873e89f9e
Alexander <tr3821ep@my.century.edu>
2018-11-05 20:00:45 -0600

    isStudent method and intro message created

--- src/edu/century/groupProject/DriverGUI.java
+++ src/edu/century/groupProject/DriverGUI.java
@@ -10,9 +10,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.GregorianCalendar;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
 
@@ -62,6 +64,17 @@ public class DriverGUI extends JFrame implements ActionListener{
 			System.out.println("The Student Doesn't Exist!!\n");
 		}
 	}
+	
+	public int isStudent() {
+    	String[] isStudent = {"RETURNING STUDENT", "NEW STUDENT"};
+    	
+    	//Display message to players if they would like to play again/see scores
+    	int isStudentPane = JOptionPane.showOptionDialog(null,
+   		"Please Select From the Following Options", "Student Registration",
+   			JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, 
+   				 null, isStudent, isStudent[0]);
+		return isStudentPane;
+	}
     //main method
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -77,24 +90,30 @@ public class DriverGUI extends JFrame implements ActionListener{
 	}
 	
 	public DriverGUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		setBounds(500, 500, 500, 500);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
-		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(2, 0));
+		//display welcome message
+    	if(isStudent() == 0) {//if existing student
+    		//temp set to exit for building purposes
+    		System.exit(0);
+    	}else {//if new student create new JFrame for creating new student
+    		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    		setExtendedState(JFrame.MAXIMIZED_BOTH); 
+    		setBounds(500, 500, 500, 500);
+    		contentPane = new JPanel();
+    		contentPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+    		setContentPane(contentPane);
+    		contentPane.setLayout(new GridLayout(2, 0));
 		
-		JPanel topPanel = new JPanel();
-		contentPane.add(topPanel);
-		topPanel.setLayout(new GridLayout(0,1,20,20));
+    		JPanel topPanel = new JPanel();
+    		contentPane.add(topPanel);
+    		topPanel.setLayout(new GridLayout(0,1,20,20));
 		
-		//text area to display info to user
-		JPanel bottomPanel = new JPanel();
-		contentPane.add(bottomPanel);
-		bottomPanel.setLayout(new BorderLayout(0, 0));
-		outputArea = new JTextArea();
-		bottomPanel.add(new JScrollPane(outputArea), BorderLayout.CENTER);
+    		//text area to display info to user
+    		JPanel bottomPanel = new JPanel();
+    		contentPane.add(bottomPanel);
+    		bottomPanel.setLayout(new BorderLayout(0, 0));
+    		outputArea = new JTextArea();
+    		bottomPanel.add(new JScrollPane(outputArea), BorderLayout.CENTER);
+    	}
 	}
 	//action method to determine what buttons do what
 		public void actionPerformed(ActionEvent e) {
